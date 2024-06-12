b57042592910a8074a753ce513ad11e0df5a9b62
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-06-12 17:05:06 -0300

    feat: menu header responsivo mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,23 +3,61 @@ import userIcon from '../../assets/icons/mdi:account-alert-outline.svg'
 import searchIcon from '../../assets/icons/akar-icons:search.svg'
 import heartIcon from '../../assets/icons/akar-icons:heart.svg'
 import cartIcon from '../../assets/icons/ant-design:shopping-cart-outlined.svg'
-
+import menuToggle from '../../assets/icons/toggleMenu.svg'
 import { Button } from '../Button'
 import styles from './index.module.css'
+import { useState } from 'react'
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <nav>
+        <MenuMobile />
+        <Menu />
+      </nav>
+    </header>
+  )
+}
+
+function MenuMobile() {
+  const [menuMobile, setMenuMobile] = useState(false)
+
+  return (
+    <div className={styles.menuMobile}>
+      <button
+        onClick={() => setMenuMobile((state) => !state)}
+        className={styles.buttonToggle}
+      >
+        <img height={41} src={menuToggle} alt="menu" />
+      </button>
+      {menuMobile && <MenuContainer />}
+    </div>
+  )
+}
+
+function Menu() {
+  return (
+    <>
+      <div className={styles.menu}>
+        <MenuContainer />
+      </div>
+    </>
+  )
+}
+
+function MenuContainer() {
+  return (
+    <>
       <a href="#" className={styles.logo}>
         <img height={41} src={hug} alt="#" />
       </a>
-      <nav className={styles.nav}>
+      <section className={styles.section}>
         <Button>Home</Button>
         <Button>Shop</Button>
         <Button>About</Button>
         <Button>Contact</Button>
-      </nav>
-      <nav className={styles.nav}>
+      </section>
+      <section className={`${styles.section} ${styles.containerIconsMobile}`}>
         <Button>
           <img src={userIcon} alt="#" />
         </Button>
@@ -32,7 +70,7 @@ export function Header() {
         <Button>
           <img src={cartIcon} alt="#" />
         </Button>
-      </nav>
-    </header>
+      </section>
+    </>
   )
 }
